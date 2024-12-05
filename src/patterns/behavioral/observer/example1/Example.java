557668b67b6c6d7f83c80f23cab860a4006c7037
Numseinemmeunome <112131011+Numseinemmeunome@users.noreply.github.com>
2024-12-05 00:28:29 -0300

    Exemplo e readme

---
+++ src/patterns/behavioral/observer/example1/Example.java
@@ -0,0 +1,72 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+//a classe Subject mantém uma lista de observadores e notifica-os quando seu estado muda.
+class Subject {
+  private List<Observer> observers = new ArrayList<Observer>();
+
+  //adiciona um observador à lista de observadores.
+  public void addObserver(Observer observer) {
+    observers.add(observer);
+  }
+  
+  //remove um observador da lista
+  public void removeObserver(Observer observer) {
+    observers.remove(observer);
+  }
+  
+  //Notifica todos os observadores na lista que o Subject mudou de estado.
+  public void notifyObservers() {
+    Iterator it = observers.iterator();
+    while (it.hasNext()) {
+      Observer obs= (Observer) it.next();
+      obs.update(this); 
+    }
+  }
+  
+}
+
+//esta interface representa um observador no padrão de projeto Observador.
+interface Observer {
+  //este método é chamado quando a temperatura observada muda de estado.
+  public void update(Subject s);
+}
+
+
+//esta classe estende a classe Subject e representa uma temperatura no padrão de projeto Observador.
+class Temperatura extends Subject  {
+   private double temp;
+   
+   //retorna o valor de temperatura atual.
+   public double getTemp() {
+     return temp;
+   }
+   
+   //eefine o novo valor de temperatura e notifica todos os observadores.
+   public void setTemp(double temp) {
+     this.temp = temp;
+     notifyObservers();
+   }
+}
+
+//essa classe é um termômetro em Celsius, que implementa a interface Observer e imprime o valor em Celsius
+class TermometroCelsius implements Observer  {
+    
+  //este método é chamado quando a temperatura observada muda de estado.
+  public void update(Subject s) {
+    double temp = ((Temperatura) s).getTemp();
+    System.out.println("Temperatura Celsius: " + temp);
+  }
+}
+
+public class Main {
+ // o método principal cria uma nova temperatura, adiciona um observador de termômetro em Celsius, e define um novo valor de temperatura.
+  public static void  main(String [] args) {
+      Temperatura t = new Temperatura();
+      t.addObserver(new TermometroCelsius ());
+      t.setTemp(100.0);
+  }
+
+}
---
+++ src/patterns/behavioral/observer/example1/README.md
@@ -0,0 +1,13 @@
+# Design Pattern: Observer
+## Conceito
+Esse padrão define como implementar uma relação do tipo um-para-muitos entre objetos sujeito e observadores. Quando o estado de um sujeito muda, seus observadores devem ser notificados.
+### Problema
+Quando se quer implementar uma relação do tipo um-para-muitos entre objetos sujeito e observadores, se usa o Design Pattern Observer.
+
+## Vantagens
+* Ele não acopla os sujeitos a seus observadores
+* Uma vez implementado, o padrão Observador disponibiliza um mecanismo de notificação que pode ser reusado por diferentes pares de sujeito-observador.
+
+## Desvantagens
+* Quanto mais observadores mais complexo o código se torna.
+* Não existe uma ordem garantida para as notificações.
